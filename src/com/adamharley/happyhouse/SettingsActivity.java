8a6f91e868a7078a4e2ca541aaddbd038f87ca0a
Adam Harley <github@adamharley.co.uk>
2013-08-20 01:51:20 +0100

    avoid fragments for backcompat below Honeycomb

--- src/com/adamharley/happyhouse/SettingsActivity.java
+++ src/com/adamharley/happyhouse/SettingsActivity.java
@@ -1,33 +1,20 @@
 package com.adamharley.happyhouse;
 
-import android.annotation.TargetApi;
 import android.os.Build;
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
-import android.preference.PreferenceFragment;
 
-@TargetApi(Build.VERSION_CODES.HONEYCOMB)
 public class SettingsActivity extends PreferenceActivity {
-	public static class SettingsFragment extends PreferenceFragment {
-	    @Override
-	    public void onCreate(Bundle savedInstanceState) {
-	        super.onCreate(savedInstanceState);
-
-	        // Load the preferences from an XML resource
-	        addPreferencesFromResource(R.xml.preferences);
-	    }
-	}
-	
 	// TODO Pass volume changes back to the main activity immediately
 	// http://developer.android.com/guide/topics/ui/settings.html#Listening
 	
+	@SuppressWarnings("deprecation")
 	@Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         
-        // Display the fragment as the main content.
-        getFragmentManager().beginTransaction()
-                .replace(android.R.id.content, new SettingsFragment())
-                .commit();
+        if (Build.VERSION.SDK_INT<Build.VERSION_CODES.HONEYCOMB) {
+            addPreferencesFromResource(R.xml.preferences);
+          }
     }
 }
